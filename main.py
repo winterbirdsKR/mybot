992d375bc0d5f1743e8dd940022d8f34b95b3dde
winterbirdsKR <54500012+winterbirdsKR@users.noreply.github.com>
2022-10-22 17:01:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import discord
-import sys
 import os
+import sys
 
 intents = discord.Intents.default()
+intents.members = True
 client = discord.Client(intents=intents)
 
 
@@ -20,18 +21,23 @@ async def on_message(message):
 
     if message.content == '!음지세팅':
         if message.channel.id == 1031373432501305397:
-            await message.channel.send('!입장 [비밀번호], !퇴장 [비밀번호]')
+            await message.channel.send('사용방법 : 봇과의 DM채널에 아래 명령어 입력')
+            await message.channel.send('!입장 [비밀번호]')
+            await message.channel.send('!퇴장 [비밀번호]')
             await message.delete()
     
-    if message.channel.id == 1031373432501305397:
+    if str(message.channel.type) == 'private':
+        password = os.environ["PASSWORD"]
         server = client.get_guild(1029765705203204201)
         role = server.get_role(1031372933895032992)
-        if message.content == '!입장 11131112':
-            await message.author.add_roles(role)
-        elif message.content == '!퇴장 11131112':
-            await message.author.remove_roles(role)
-        
-        await message.delete()
+        user = server.get_member(message.author.id)
+        if message.content == '!입장 ' + password:
+            await user.add_roles(role)
+        elif message.content == '!퇴장 '+ password:
+            await user.remove_roles(role)
+
+            
+
             
 token = os.environ["BOT_TOKEN"]
 
